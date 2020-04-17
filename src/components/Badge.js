9d715d05ecb9ea7a69fb14f2fa6f2bddc84017b2
Manuel Goncalves <man281192@gmail.com>
2020-04-16 23:37:02 -0400

    introduce props to component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,6 +3,7 @@ import confLogo from '../images/mountain.png'
 import "./styles/Badge.css"
 
 class Badge extends React.Component{
+
     render(){
         return(
          <div className="Badge" >
@@ -12,12 +13,12 @@ class Badge extends React.Component{
             <div className="Badge__section-name">
                 <img className="Badge__avatar" src="https://s.gravatar.com/avatar/cf9ef2d26fc2ec042d761a2ad3d9d379?s=80" alt="Avatar"></img>
                 <h1>
-                    Manuel <br/> Goncalves
+                    {this.props.firstName} <br/> {this.props.lastName}
                 </h1>
             </div>
             <div className="Badge__section-info">
-                <h3>Frontend Engineer</h3>
-                <p>@manuel28g</p>
+                <h3>{this.props.jobTitle}</h3>
+                <p>{this.props.userName}</p>
             </div>
             <div className="Badge__footer">
                 #platziconf
