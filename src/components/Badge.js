63ed697dc6d89ee194c0c50dad4e71cdf94329dd
Manuel Goncalves <man281192@gmail.com>
2020-04-16 22:55:39 -0400

    Style to interface added

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import confLogo from '../images/mountain.png'
+import "./styles/Badge.css"
 
 class Badge extends React.Component{
     render(){
-        return <div>
-            <div> 
+        return(
+         <div className="Badge" >
+            <div className="Badge__header"> 
                 <img src={confLogo} alt="Logo de la conferencia"></img>
             </div>
-            <div>
-                <img src="https://s.gravatar.com/avatar/cf9ef2d26fc2ec042d761a2ad3d9d379?s=80" alt="Avatar"></img>
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://s.gravatar.com/avatar/cf9ef2d26fc2ec042d761a2ad3d9d379?s=80" alt="Avatar"></img>
                 <h1>
                     Manuel <br/> Goncalves
                 </h1>
             </div>
-            <div>
-                <p>Frontend Engineer</p>
+            <div className="Badge__section-info">
+                <h3>Frontend Engineer</h3>
+                <p>@manuel28g</p>
             </div>
-            <footer>
+            <div className="Badge__footer">
                 #platziconf
-            </footer>
-        </div>;
+            </div>
+        </div>);
     }
 }
 
