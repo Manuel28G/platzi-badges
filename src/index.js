c7b1a11e9f76351bd5f82b611d2801abfd19c602
Manuel Goncalves <man281192@gmail.com>
2020-05-02 15:29:22 -0400

    badge list added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import  BadgeNew from './pages/BadgeNew';
+import  Badges from './pages/Badges.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css'
 
 const container = document.getElementById('app');
-ReactDOM.render(<BadgeNew  />,container);
+ReactDOM.render(<Badges  />,container);
