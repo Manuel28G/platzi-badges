fa3e6840441fb100e5279a46836edb0d93f8a533
Manuel Goncalves <man281192@gmail.com>
2020-05-17 01:01:22 -0400

    Implement the most used method of lifecycle

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,42 +5,82 @@ import BadgeList from '../components/BadgesList.js'
 import { Link } from 'react-router-dom'
 
 class Badges extends React.Component{
-    state ={
-        data:[
-            {
-              id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-              firstName: 'Freda',
-              lastName: 'Grady',
-              email: 'Leann_Berge@gmail.com',
-              jobTitle: 'LegacyBrandDirector',
-              twitter: 'FredaGrady22221-7573',
-              avatarUrl:
-                'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-            },
-            {
-              id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-              firstName: 'Major',
-              lastName: 'Rodriguez',
-              email: 'Ilene66@hotmail.com',
-              jobTitle: 'HumanResearchArchitect',
-              twitter: 'MajorRodriguez61545',
-              avatarUrl:
-                'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-            },
-            {
-              id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-              firstName: 'Daphney',
-              lastName: 'Torphy',
-              email: 'Ron61@hotmail.com',
-              jobTitle: 'NationalMarketsOfficer',
-              twitter: 'DaphneyTorphy96105',
-              avatarUrl:
-                'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-            },
-          ],
-    };
+   
+
+
+    constructor(props){
+        //space to initialize variables or states
+        super(props)
+        console.log('1. constructor()');
+        this.state ={
+            data:[],
+        };
+    }
+
+    componentWillUnmount(){
+        console.log('final. componentWillUnmount()');
+        //we will clean variable because if a component will be unmount
+        //and (in this example) the timeout is not exec, when it execute the routine
+        //be throw a error in console
+        clearTimeout(this.timeoutId);
+    }
+
+    componentDidUpdate(prevProps,prevState){
+
+        console.log('5. componentDidUpdate()');
+        console.log({
+            prevProps:prevProps,
+            prevState:prevState
+        });
+        console.log({
+            prevProps:this.props,
+            prevState:this.state
+        });
+    }
+
+
+    componentDidMount(){
+        console.log('3. componentDidMount()');
+        this.timeoutId = setTimeout(() => {
+            this.setState({
+                data:[
+                {
+                  id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+                  firstName: 'Freda',
+                  lastName: 'Grady',
+                  email: 'Leann_Berge@gmail.com',
+                  jobTitle: 'LegacyBrandDirector',
+                  twitter: 'FredaGrady22221-7573',
+                  avatarUrl:
+                    'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
+                },
+                {
+                  id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+                  firstName: 'Major',
+                  lastName: 'Rodriguez',
+                  email: 'Ilene66@hotmail.com',
+                  jobTitle: 'HumanResearchArchitect',
+                  twitter: 'MajorRodriguez61545',
+                  avatarUrl:
+                    'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
+                },
+                {
+                  id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+                  firstName: 'Daphney',
+                  lastName: 'Torphy',
+                  email: 'Ron61@hotmail.com',
+                  jobTitle: 'NationalMarketsOfficer',
+                  twitter: 'DaphneyTorphy96105',
+                  avatarUrl:
+                    'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
+                },
+            ]})
+        }, 3000);
+    }
 
     render(){
+
+        console.log('2. render()');
         return (
         <React.Fragment>
             <div className="Badges">
